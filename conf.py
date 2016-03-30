87757fcc516850f351add24948606be5cd9d9d6c
Eric Han Liu <ericliu404@gmail.com>
2016-03-29 22:58:59 -0400

    Remove personal information from conf

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-TO = "+14388202051" #put the number you want the message to send to
-FROM = "+14387938743" #put your twilio phone number
-ACCOUNT_SID = "ACad58a5c39c9e59b44703c80d49381689" #put your account sid
-AUTH_TOKEN = "58fab1073732de062a79c17f86e9862c" #put your authentication token (click the lock to view)
+TO = "+15555555555" #put the number you want the message to send to
+FROM = "+15555555555" #put your twilio phone number
+ACCOUNT_SID = "bananas" #put your account sid
+AUTH_TOKEN = "limes" #put your authentication token (click the lock to view)
